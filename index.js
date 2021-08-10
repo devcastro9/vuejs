71f0c451942cd651d133a2ec0f08f99ee015c048
CGI <castro.q.orlando@gmail.com>
2021-08-10 08:58:21 -0400

    new_app-vuejs

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!'
     }
-});
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'Fecha: ' +  new Date().toLocaleDateString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
